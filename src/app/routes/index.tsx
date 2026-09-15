b32e819ca941d98fcce52ccd638448044d1b7e54
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2026-09-15 11:57:29 +0300

    handle routes and global css conflict

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,33 +1,19 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { MainLayout } from '@/components/layout';
-<<<<<<< HEAD
-=======
-import ProfileLayout from '@/components/layout/ProfileLayout';
->>>>>>> ee1d787 (Merge pull request #15 from Abdulrahmanfawzy/profile)
-import CartPage from '@/features/cart/pages/CartPage';
 
 // Auth Pages Imports
-<<<<<<< HEAD
-import Login from '@/auth/Login';
-import ForgotPassword from '@/auth/ForgotPassword';
-import VerifyOTP from '@/auth/VerifyOTP';
-import ResetPassword from '@/auth/ResetPassword';
-import Signup from '@/auth/Signup';
-import HomePage from '@/pages/HomePage';
-import ProductList from '@/pages/ProductList';
-import ProductDetails from '@/pages/ProductDetails';
-import Category from '@/features/category';
-=======
+import Signup from '@/features/auth/Signup';
 import Login from '@/features/auth/Login';
 import ForgotPassword from '@/features/auth/ForgotPassword';
 import VerifyOTP from '@/features/auth/VerifyOTP';
 import ResetPassword from '@/features/auth/ResetPassword';
-import Signup from '@/features/auth/Signup';
-
-// Profile pages Imports
+import ProductList from '@/pages/ProductList';
+import ProductDetails from '@/pages/ProductDetails';
+import Category from '@/features/category';
+import HomePage from '@/features/home';
+import ProfileLayout from '@/components/layout/ProfileLayout';
 import { PersonalInfo } from '@/profile/PersonalInfo';
-
->>>>>>> ee1d787 (Merge pull request #15 from Abdulrahmanfawzy/profile)
+import CartPage from '@/features/cart/pages/CartPage';
 
 export function AppRoutes() {
   return (
@@ -40,9 +26,8 @@ export function AppRoutes() {
       <Route path="/verify-otp" element={<VerifyOTP />} />
       <Route path="/reset-password" element={<ResetPassword />} />
 
-      {/* Main App Routes (With Navbar & Footer) */}
+      {/* Main App Routes (Inside MainLayout) */}
       <Route element={<MainLayout />}>
-        {/* Profile Routes */}
         <Route path="/profile" element={<ProfileLayout />}>
           <Route index element={<PersonalInfo />} />
           <Route path="personal-info" element={<PersonalInfo />} />
@@ -51,19 +36,8 @@ export function AppRoutes() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/productlist" element={<ProductList />} />
         <Route path="/productdetails/:id" element={<ProductDetails />} />
-<<<<<<< HEAD
-        <Route path='/categories' element={<Category/> }/>
-=======
->>>>>>> ee1d787 (Merge pull request #15 from Abdulrahmanfawzy/profile)
+        <Route path='/categories' element={<Category />} />
       </Route>
     </Routes>
   );
 }
-
-
-
-
-
-
-
-
