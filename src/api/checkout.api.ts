7b47d06fdbb486d646371267bc3ff6517e0c837e
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2026-09-19 15:23:16 +0300

    checkout fixed error

--- src/api/checkout.api.ts
+++ src/api/checkout.api.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import { api } from '../lib/axios'
 import type { CheckoutPayload, CheckoutResponse } from '@/features/checkout/types/checkout'
-=======
-import type { CheckoutPayload, CheckoutResponse } from '@/features/checkout/types/checkout';
-import api from '@/utils/axios';
-
->>>>>>> 44f1be47d5fb6ce997b837e6f8156cc31859f7b0
-
+import api from '@/utils/axios'
 export const checkoutApi = {
   createCheckout: async (payload: CheckoutPayload): Promise<CheckoutResponse> => {
     const response = await api.post<CheckoutResponse>('/checkout', payload)
